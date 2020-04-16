fca122df828098f774f5e2e376efa16b85172489
David Severski <davidski@deadheaven.com>
2020-04-16 08:20:09 +0000

    version 0.4.2

--- inst/doc/process.R
+++ inst/doc/process.R
@@ -1,10 +1,10 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  base_dir <- "~/evaluator"
 #  source("~/evaluator/run_analysis.R")
 
--- inst/doc/usage.R
+++ inst/doc/usage.R
@@ -1,36 +1,36 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 library(tibble)
 library(dplyr)
 library(pander)
 
-## ----quick_start, eval=FALSE---------------------------------------------
+## ----quick_start, eval=FALSE--------------------------------------------------
 #  base_dir <- "~/evaluator"
 #  source("~/evaluator/run_analysis.R")
 
-## ----import, eval=FALSE--------------------------------------------------
+## ----import, eval=FALSE-------------------------------------------------------
 #  domains <- readr::read_csv("~/evaluator/inputs/domains.csv")
 #  import_spreadsheet("~/evaluator/inputs/survey.xlsx", domains, output_dir = "~/evaluator/inputs")
 
-## ----validate, eval=FALSE------------------------------------------------
+## ----validate, eval=FALSE-----------------------------------------------------
 #  qualitative_scenarios <- readr::read_csv("~/evaluator/inputs/qualitative_scenarios.csv")
 #  mappings <- readr::read_csv("~/evaluator/inputs/qualitative_mappings.csv")
 #  capabilities <- readr::read_csv("~/evaluator/inputs/capabilities.csv")
 #  validate_scenarios(qualitative_scenarios, capabilities, domains, mappings)
 
-## ----encode, eval = FALSE------------------------------------------------
+## ----encode, eval = FALSE-----------------------------------------------------
 #  quantitative_scenarios <- encode_scenarios(qualitative_scenarios,
 #                                             capabilities, mappings)
 
-## ----simulate, eval = FALSE----------------------------------------------
+## ----simulate, eval = FALSE---------------------------------------------------
 #  simulation_results <- run_simulations(quantitative_scenarios,
 #                                        iterations = 100L)
 #  saveRDS(simulation_results, file = "~/evaluator/results/simulation_results.rds")
 
-## ----summarize, eval=FALSE-----------------------------------------------
+## ----summarize, eval=FALSE----------------------------------------------------
 #  summarize_to_disk(simulation_results = simulation_results,
 #                    results_dir = "~/evaluator/results")
 
-## ----data_files, eval = TRUE, echo=FALSE---------------------------------
+## ----data_files, eval = TRUE, echo=FALSE--------------------------------------
 tibble::tribble(
     ~"Data File", ~Purpose,
     "simulation_results.rds", "Full details of each simulated scenario",
@@ -38,7 +38,7 @@ tibble::tribble(
     "domain_summary.rds", "Simulation results, summarized at the domain level"
 ) %>% pander::pander(., justify = "left")
 
-## ----analyze, eval=FALSE-------------------------------------------------
+## ----analyze, eval=FALSE------------------------------------------------------
 #  # Explorer
 #  explore_scenarios(input_directory = "~/evaluator/inputs",
 #                    results_directory = "~/evaluator/results")
